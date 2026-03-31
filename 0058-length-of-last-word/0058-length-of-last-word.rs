4d9fc20ba07f38a601fc672f694ec449d53e525e
Vamsi Krishna Reddy A K <83443298+VAMSIKRISHNA-Optimization@users.noreply.github.com>
2026-04-01 00:09:30 +0530

    Time: 0 ms (100%), Space: 2.2 MB (60.46%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.rs
+++ 0058-length-of-last-word/0058-length-of-last-word.rs
@@ -1,5 +1,6 @@
 impl Solution {
-    pub fn length_of_last_word(s: String) -> i32 {
-        
+    pub fn length_of_last_word(s: String) -> i32 
+    {
+        s.trim().split_whitespace().last().expect("FAIL").len() as i32
     }
 }
