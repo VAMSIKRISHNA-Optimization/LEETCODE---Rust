0b41941343b106197b8d3ceed60e2dc85cc0161d
Vamsi Krishna Reddy A K <83443298+VAMSIKRISHNA-Optimization@users.noreply.github.com>
2026-06-13 19:11:51 +0530

    Time: 0 ms (100%), Space: 2.2 MB (42.55%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.rs
@@ -0,0 +1,38 @@
+impl Solution 
+{
+    pub fn add_strings(num1: String, num2: String) -> String 
+    {
+        let s1 = num1.as_bytes();
+        let s2 = num2.as_bytes();
+        
+        let mut i = s1.len() as i32 - 1;
+        let mut j = s2.len() as i32 - 1;
+        
+        let mut carry = 0;
+        let mut result = String::new();
+
+        while i >= 0 || j >= 0 || carry > 0 
+        {
+            let mut sum = carry;
+
+            if i >= 0 
+            {
+                sum += (s1[i as usize] - b'0') as i32;
+                i -= 1;
+            }
+
+            if j >= 0 
+            {
+                sum += (s2[j as usize] - b'0') as i32;
+                j -= 1;
+            }
+
+            carry = sum / 10;
+            let digit = (sum % 10) as u8 + b'0';
+
+            result.push(digit as char);
+        }
+
+        result.chars().rev().collect()
+    }
+}
